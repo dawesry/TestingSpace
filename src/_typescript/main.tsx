3fae4b34cb1fe8b2c42a6372d4fb8be4c0fe2fcc
dawesry <git@dawes.cc>
2022-07-06 14:48:14 -0400

    update

--- src/_typescript/main.tsx
+++ src/_typescript/main.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
-import BasicAlerts from './alert';
+import BasicAlerts from './modules/alert';
 
 ReactDOM.createRoot(document.querySelector("#alert")).render(
   <React.StrictMode>
