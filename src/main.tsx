2b80d5099f9f0f5a5e812ab941c83e7c0f99c6df
dawesry <git@dawes.cc>
2022-07-07 12:29:14 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,11 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import BasicAlerts from './_typescript/alert';
-import TestCard from './_typescript/card';
 
-ReactDOM.createRoot(document.querySelector("#porpoalert")).render(
+ReactDOM.createRoot(document.querySelector("#devalert")).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <BasicAlerts />
-    </StyledEngineProvider>
-  </React.StrictMode>
-);
-
-ReactDOM.createRoot(document.querySelector("#testcard")).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <TestCard />
     </StyledEngineProvider>
   </React.StrictMode>
 );
