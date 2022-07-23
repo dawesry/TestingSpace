5a1b9ee54057b4a0243c37389f2bdb56ecf5831a
Ryan Dawes <git@dawes.cc>
2022-07-23 10:25:18 -0400

    remove pieces

--- src/_typescript/index.tsx
+++ src/_typescript/index.tsx
@@ -3,29 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 
 import BasicAlerts from './modules/alert';
-import NavBar from './modules/nav';
-import Featured from './modules/featured';
 
 ReactDOM.createRoot(document.querySelector("#devalert")).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <BasicAlerts />
-    </StyledEngineProvider>
-  </React.StrictMode>
-);
-
-ReactDOM.createRoot(document.querySelector("#navbar")).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <NavBar />
-    </StyledEngineProvider>
-  </React.StrictMode>
-);
-
-ReactDOM.createRoot(document.querySelector("#featured")).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <Featured />
     </StyledEngineProvider>
   </React.StrictMode>
 );
